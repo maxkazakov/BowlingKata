925656c78a4d54f2007d1be59760eb6a4ff679b0
Kirill Orloff <k.orlov@dodopizza.com>
2021-03-03 17:02:01 +0300

    Refactor score func

--- BowlingKata/Game.swift
+++ BowlingKata/Game.swift
@@ -7,15 +7,62 @@
 
 import Foundation
 
+struct Constants {
+    static let maxNumberOfFrames = 10
+    static let rollPerFrame = 2
+}
+
 class Game {
     
-    private var currentScore = 0
+    private var currentRoll = 0
+    private var pins: [Int] = []
     
     func score() -> Int {
+        var currentScore = 0
+        
+        /*
+         r1 = 10 // score = 10
+         r2 = -
+         
+         r3 = 10 // score = 10 + 10*2
+         r4 = -
+         
+         r5 = 3 // score = 10 + 10*2 + 3*3
+         r6 = 4 // score = 10 + 10*2 + 3*3 + 4*2
+         
+         r7 = 5 // score = 10 + 10*2 + 3*3 + 4*2 + 5
+         r8
+         */
+        
+        var currentRollInFrame = 0 // 0 or 1
+        var currentFrame = 0 // from 0 to 9
+        var isSpare = false
+        
+        for (idx, currentPins) in pins.enumerated() {
+            if isSpare {
+                currentScore += currentPins * 2
+                isSpare = false
+            } else {
+                currentScore += currentPins
+            }
+            
+            if currentRollInFrame == 0 {
+                currentRollInFrame = 1
+            } else {
+                currentRollInFrame = 0
+                currentFrame += 1
+                let pinsInFrame = currentPins + pins[idx - 1]
+                isSpare = pinsInFrame == 10
+            }
+            
+        }
         return currentScore
     }
     
     func roll(_ pins: Int) {
-        currentScore = pins
+        let maxNumberOfRolls = Constants.maxNumberOfFrames * Constants.rollPerFrame
+        guard currentRoll < maxNumberOfRolls else { return }
+        currentRoll += 1
+        self.pins.append(pins)
     }
 }
--- BowlingKataTests/GameTests.swift
+++ BowlingKataTests/GameTests.swift
@@ -15,7 +15,53 @@ class GameTests: XCTestCase {
         sut = Game()
     }
     
+    func test_WhenSpareInOtherFrames_thenScoreIsCorrect() {
+        sut.roll(3)
+        sut.roll(4)
+        sut.roll(6)
+        
+        sut.roll(1)
+        
+        let score = sut.score()
+        XCTAssertEqual(score, 3 + 4 + 6 + 1)
+    }
     
+    func test_WhenOneSpare_thenScoreIsCorrect() {
+        sut.roll(3)
+        sut.roll(7)
+        
+        sut.roll(5)
+        
+        let score = sut.score()
+        XCTAssertEqual(score, 3 + 7 + 5 * 2)
+    }
+    
+    func test_WhenCallRollMaxNumberOfFrames_thenScoreIsNotChanged() {
+        // Arrange
+        let maxNumberOfRolls = Constants.maxNumberOfFrames * Constants.rollPerFrame
+        for _ in 0..<maxNumberOfRolls {
+            sut.roll(1)
+        }
+        
+        //Act
+        sut.roll(5)
+        
+        //Assert
+        let score = sut.score()
+        XCTAssertEqual(score, 1 * maxNumberOfRolls)
+    }
+    
+    func test_WhenCallRollTwice_thenScoreIsCorrect() {
+        // Arrange
+        
+        // Act
+        sut.roll(5)
+        sut.roll(5)
+        
+        // Assert
+        let score = sut.score()
+        XCTAssertEqual(score, 10)
+    }
     
     func test_WhenCallRollOnce_thenScoreIsCorrect() {
         // Arrange
